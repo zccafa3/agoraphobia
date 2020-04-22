1628499a5db0541d914d454a78a40425218016cb
Wyndham Wynne <wyndham@labminds.co.uk>
2020-04-22 23:41:14 +0100

    Fixing commsLib

--- worker/lib/commsLib.lua
+++ worker/lib/commsLib.lua
@@ -8,14 +8,13 @@ local event = require('event')
 
 local utilsLib = require('utilsLib')
 
-local event = component.event
 local tunnel = component.tunnel
 
 _ENV = commsLib
 
 --- getMasterData gets data sent from the Master device
 -- @return recieved data
-local function getMasterData()
+function commsLib.getMasterData()
   local _, _, _, _, _, recievedData = event.pull('modem_message')
   return recievedData
 end
@@ -24,25 +23,8 @@ end
 -- @param data to send
 -- @tparam string
 -- @return true, or false
-local function sendMasterData(data)
+function commsLib.sendMasterData(data)
   return tunnel.send(data)
-end
-
---- Table of communication instructs
--- @table knownInsrructs
-local knownInstructs = {
-  getMasterData   = getMasterData,
-  sendMasterData  = sendMasterData}
-
---- handleCommsInstructs executed the specified instruct with the appropriate
----number of arguements
--- @param instructStr instruction to be executed
--- @tparam string
--- @return relative returns
-function commsLib.handleCommsInstruct(instructStr)
-  local instruct, instructArgs = utilsLib.splitStrAtColon(instructStr)
-  local instructArgList = utilsLib.multiSplitStrAtColon(instructArgs)
-  return utilsLib.runFuncWithArgs(knownInstructs[instruct], instructArgList)
 end
 
 return commsLib
